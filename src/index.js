bee85b4e6b48a6ceaf97d4cb2b264ea14b0b109e
Miren Pau <miren_pau23@hotmail.co.uk>
2019-08-22 15:16:38 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const getChrome = require("./getChrome");
+const puppeteer = require("puppeteer-core");
+
+exports.handler = async (event, context, callback) => {
+    
+    // For keeping the browser launch
+    context.callbackWaitsForEmptyEventLoop = false;
+    
+    const chrome = await getChrome();
+
+    const browser = await puppeteer.connect({
+       browserWSEndpoint: chrome.endpoint
+    });
+
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "access-control-allow-methods": "GET"
+    };
+
+    var encodedTargetUrl = "https://www.google.co.uk";
+
+    if(typeof event.queryStringParameters !== "undefined"){
+        encodedTargetUrl = encodeURI(event.queryStringParameters.url)
+    }
+
+    const page = await browser.newPage();
+    const response = await page.goto(encodedTargetUrl, {
+        waitUntil: ["domcontentloaded", "networkidle0"]
+
+    });
+
+    try {
+        callback(null, {
+            statusCode: await response.status(),
+            headers,
+            body: await response.text()
+        });
+    
+        browser.close();
+
+    }catch(e){
+        callback(e); 
+    }
+
+};
